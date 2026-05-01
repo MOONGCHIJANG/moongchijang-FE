d88ac9e67335b8af4bef22387b69b9bb728c39a9
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-05-02 02:29:26 +0900

    [MCJ-1356] feat: 유의사항 텍스트 추가

--- src/app/(pages)/item/[groupBuyId]/page.tsx
+++ src/app/(pages)/item/[groupBuyId]/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import TopInfo from './_components/TopInfo';
 import ItemDetail from './_components/ItemDetail';
+import GuideLine from './_components/GuideLine';
 
 const page = () => {
+  // getGetApiV1GroupBuysGroupBuyIdMockHandler
   return (
     <div className="">
       <TopInfo />
       <ItemDetail />
+      <GuideLine />
     </div>
   );
 };

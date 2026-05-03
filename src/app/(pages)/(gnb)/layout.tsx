282ea947d496d416d91c92cc2c98b0a1938d6976
leemanjae02 <honthalg@gmail.com>
2026-05-03 15:00:30 +0900

    [MCJ-1373] refactor:  footer 너비제한 제거
    - 부모 div에 최소, 최대 너비가 명시되어 있기때문에 w-full만 놔두고 제거했습니다.

--- src/app/(pages)/(gnb)/layout.tsx
+++ src/app/(pages)/(gnb)/layout.tsx
@@ -4,7 +4,7 @@ export default function GnbLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative mx-auto flex h-dvh w-full min-w-[360px] max-w-[440px] flex-col overflow-hidden bg-surface-default">
       <main className="flex-1 overflow-y-auto">{children}</main>
-      <footer className="w-full min-w-[360px] max-w-[440px]">
+      <footer className="w-full">
         <GNB />
       </footer>
     </div>

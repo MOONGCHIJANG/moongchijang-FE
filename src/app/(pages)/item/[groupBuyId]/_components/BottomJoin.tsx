9c32581a943dc819d14f52ca4f4e5156fefe689c
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-05-04 22:09:32 +0900

    [MCJ-1391] feat: 공구 참여하기 페이지 이동

--- src/app/(pages)/item/[groupBuyId]/_components/BottomJoin.tsx
+++ src/app/(pages)/item/[groupBuyId]/_components/BottomJoin.tsx
@@ -53,6 +53,12 @@ const BottomJoin = ({ data }: Props) => {
     }
   };
 
+  const handleJoin = () => {
+    // TODO: 로그인한 사용자만 참여 가능하도록 수정 필요
+    if (isExpired) return;
+    window.location.href = `/item/${data.id}/join`;
+  };
+
   return (
     <div className="fixed bottom-0 left-0 right-0 z-10 mx-auto w-full max-w-110 px-7 py-p6 flex flex-col gap-g3 rounded-t-3xlarge shadow-[0px_-2px_20px_0px_rgba(0,0,0,0.10)] bg-white">
       <p className="text-center text-brand-primary body-md-bold">
@@ -69,6 +75,7 @@ const BottomJoin = ({ data }: Props) => {
           size="md"
           className="w-full text-white cursor-pointer"
           disabled={isExpired}
+          onClick={handleJoin}
         >
           공구 참여하기
         </Button>

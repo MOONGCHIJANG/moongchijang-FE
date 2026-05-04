ecf5bc2a84be032bb9dca0f854794f59044455aa
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-05-05 01:05:36 +0900

    [MCJ-1391] feat: 전화번호 입력 시트

--- src/app/(pages)/item/[groupBuyId]/join/_components/JoinForm.tsx
+++ src/app/(pages)/item/[groupBuyId]/join/_components/JoinForm.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import NumStepper from './NumStepper';
 import { Icon } from '@iconify/react';
+import EditNumber from './EditNumber';
 
 const JoinForm = () => {
+  const [open, setOpen] = useState(false);
+  // 실제 데이터로 변경
+  const [phoneNumber, setPhoneNumber] = useState('010-0000-0000');
+
   return (
     <div className="flex flex-col gap-3 pt-3">
+      {/* 상품 카드 */}
       <div className="rounded-2xlarge bg-surface-white border border-border-subtle p-p5 flex items-center justify-between">
         <div className="flex gap-g4 items-center">
           <div className="caption-sm-regular w-15 h-15 rounded-xlarge bg-gray-200">
@@ -17,18 +24,29 @@ const JoinForm = () => {
         </div>
         <NumStepper max={10} />
       </div>
+      {/* 전화번호 */}
       <div className="rounded-2xlarge bg-surface-white border border-border-subtle p-p5 flex flex-col gap-g4">
         <p className="heading-md-bold">전화번호</p>
         <div className="flex justify-between items-center">
-          <p className="body-md-regular">010-0000-0000</p>
-          <button type="button">
+          <p className="body-md-regular">{phoneNumber}</p>
+          <button type="button" onClick={() => setOpen(true)}>
             <Icon
               icon="lucide:chevron-right"
               className="w-6 h-6 text-icon-subtle"
             />
           </button>
         </div>
+        <EditNumber
+          open={open}
+          initialValue={phoneNumber}
+          onClose={() => setOpen(false)}
+          onConfirm={(value) => {
+            setPhoneNumber(value);
+            setOpen(false);
+          }}
+        />
       </div>
+      {/* 결제 정보 */}
       <div className="rounded-2xlarge bg-surface-white border border-border-subtle p-p5 flex flex-col gap-g4">
         <p className="heading-md-bold">결제 금액</p>
         <div className="flex flex-col gap-g5">

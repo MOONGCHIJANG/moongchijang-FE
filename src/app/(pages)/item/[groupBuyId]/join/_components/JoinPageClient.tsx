cd27449f8c7e996b87a495b54cc6b601ee15cc80
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-05-05 01:32:46 +0900

    [MCJ-1391] feat: 동의 및 결제 버튼 구역

--- src/app/(pages)/item/[groupBuyId]/join/_components/JoinPageClient.tsx
+++ src/app/(pages)/item/[groupBuyId]/join/_components/JoinPageClient.tsx
@@ -10,7 +10,7 @@ const JoinPageClient = () => {
       <ItemSummary />
       <JoinForm />
       <AgreeTerms />
-      <PaymentButton />
+      <PaymentButton price={18000} />
     </div>
   );
 };
--- src/app/(pages)/item/[groupBuyId]/join/_components/PaymentButton.tsx
+++ src/app/(pages)/item/[groupBuyId]/join/_components/PaymentButton.tsx
@@ -1,7 +1,20 @@
+import { Button } from '@/components/Button';
 import React from 'react';
 
-const PaymentButton = () => {
-  return <div>PaymentButton</div>;
+type Props = {
+  price: number;
+};
+
+const PaymentButton = ({ price }: Props) => {
+  return (
+    <div className="fixed z-20 bottom-0 left-0 right-0 w-full mx-auto max-w-110">
+      <div className="p-4">
+        <Button size="lg" className="" fullWidth>
+          {price.toLocaleString()}원 결제하기
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default PaymentButton;

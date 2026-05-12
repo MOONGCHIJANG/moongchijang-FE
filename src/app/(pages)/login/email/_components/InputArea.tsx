b72e1c56feea41c3952944fd305d2a2b3b65ee71
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-05-13 02:02:04 +0900

    [MCJ-1427] feat: 이메일 로그인 페이지

---
+++ src/app/(pages)/login/email/_components/InputArea.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@/components/Button';
+import Input from '@/components/Input';
+import React from 'react';
+
+const InputArea = () => {
+  return (
+    <>
+      <div className="flex flex-col gap-g4 w-full">
+        <Input label="이메일 주소" placeholder="이메일 주소를 입력해주세요." />
+        <Input
+          label="비밀번호"
+          isPassword
+          placeholder="비밀번호를 입력해주세요."
+          helperText="영문, 숫자, 특수문자가 모두 들어간 8자 이상"
+        />
+      </div>
+      <div className="flex flex-col gap-g5 w-full">
+        <Button variant="primary" size="lg" className="w-full">
+          로그인
+        </Button>
+        <div className="w-full flex items-center justify-center text-text-teritary body-md-regular">
+          <button>아이디/비밀번호 찾기</button>
+          <span className="mx-g5 text-border-subtle">|</span>
+          <button>회원가입</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default InputArea;

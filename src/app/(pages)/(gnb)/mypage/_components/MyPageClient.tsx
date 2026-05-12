c156e6d1639acce3b5197de271c9fa00f6f04bc4
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-05-12 23:43:39 +0900

    [MCJ-1427] feat: 로그인 상태 분기

--- src/app/(pages)/(gnb)/mypage/_components/MyPageClient.tsx
+++ src/app/(pages)/(gnb)/mypage/_components/MyPageClient.tsx
@@ -1,26 +1,37 @@
 import { Icon } from '@iconify/react';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const MyPageClient = () => {
+  // TODO: 추후 실제 로그인 상태로 적용
+  const isLoggedIn = false;
+
   return (
     <>
       {/* 비로그인 상태일 때 */}
-      <div className="h-dvh bg-surface-white no-scroll">
-        <div className="py-4 bg-bg-white-muted">
-          <button className="flex justify-between w-full items-center p-g5 bg-surface-white">
-            <p className="heading-sm-semibold">로그인하고 뭉치장 이용하기</p>
-            <Icon
-              icon="lucide:chevron-right"
-              className="w-8 h-8 text-icon-basic"
-            />
-          </button>
+      {!isLoggedIn && (
+        <div className="h-dvh bg-surface-white no-scroll">
+          <div className="py-4 bg-bg-white-muted">
+            <Link
+              href="/login"
+              className="flex justify-between w-full items-center p-g5 bg-surface-white"
+            >
+              <p className="heading-sm-semibold">로그인하고 뭉치장 이용하기</p>
+              <Icon
+                icon="lucide:chevron-right"
+                className="w-8 h-8 text-icon-basic"
+              />
+            </Link>
+          </div>
+          <div className="flex flex-col gap-g5 items-center pt-25">
+            <Image src="/icons/starpoint.svg" width={91} height={91} alt="" />
+            <p className="whitespace-pre-line heading-sm-medium text-center">{`로그인 후\n이용 가능해요`}</p>
+          </div>
         </div>
-        <div className="flex flex-col gap-g5 items-center pt-25">
-          <Image src="/icons/starpoint.svg" width={91} height={91} alt="" />
-          <p className="whitespace-pre-line heading-sm-medium text-center">{`로그인 후\n이용 가능해요`}</p>
-        </div>
-      </div>
+      )}
+      {/* 로그인 상태일 때 */}
+      {isLoggedIn && <div>로그인완료 시 마이페이지</div>}
     </>
   );
 };

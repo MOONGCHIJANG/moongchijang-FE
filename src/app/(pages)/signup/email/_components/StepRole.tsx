1da6b70d8690bc07a51ae1b1ad5d525dae6d5a3a
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-05-20 19:00:53 +0900

    [MCJ-1467] feat: 손님/사장님 분기 페이지

--- src/app/(pages)/signup/email/_components/StepRole.tsx
+++ src/app/(pages)/signup/email/_components/StepRole.tsx
@@ -1,3 +1,6 @@
+import { Button } from '@/components/Button';
+import { Icon } from '@iconify/react';
+import Image from 'next/image';
 import React from 'react';
 
 type StepRoleProps = {
@@ -6,10 +9,64 @@ type StepRoleProps = {
 
 const StepRole = ({ onBack }: StepRoleProps) => {
   return (
-    <div>
-      <h2>역할 선택</h2>
-      <button onClick={onBack}>이전</button>
-    </div>
+    <>
+      <div className="flex justify-between items-start pb-g12">
+        <p className="heading-lg-bold whitespace-pre-line">
+          {`손님이신가요?
+              사장님이신가요?`}
+        </p>
+        <Image
+          src="/icons/progress-3.png"
+          alt="progress"
+          width={512}
+          height={113}
+          className="w-32.75 h-7.2"
+        />
+      </div>
+      <div className="flex flex-col gap-6.5 pb-18">
+        <button className="px-3.75 flex rounded-[17.6px] h-26.75 border border-border-subtle items-center">
+          <Image
+            src="/images/customer.svg"
+            alt="customer"
+            width={61}
+            height={52}
+          />
+          <div className="flex flex-col gap-g2 px-p5 flex-1 items-start">
+            <p className="heading-lg-bold">손님이에요</p>
+            <p className="heading-sm-regular text-text-tertiary">
+              공구 참여하고 개설 요청하기
+            </p>
+          </div>
+          <Icon
+            icon="lucide:chevron-right"
+            width={24}
+            height={24}
+            className="text-icon-basic"
+          />
+        </button>
+        <button className="px-3.75 flex rounded-[17.6px] h-26.75 border border-border-subtle items-center">
+          <Image src="/images/owner.svg" alt="owner" width={61} height={52} />
+          <div className="flex flex-col gap-g2 px-p5 flex-1 items-start">
+            <p className="heading-lg-bold">사장님이에요</p>
+            <p className="heading-sm-regular text-text-tertiary">
+              공구 관리하기
+            </p>
+          </div>
+          <Icon
+            icon="lucide:chevron-right"
+            width={24}
+            height={24}
+            className="text-icon-basic"
+          />
+        </button>
+      </div>
+
+      <div className="fixed left-0 right-0 bottom-4 px-4 w-full max-w-md mx-auto z-10">
+        <Button onClick={onBack} className="" fullWidth size="lg">
+          가입 완료
+        </Button>
+      </div>
+    </>
   );
 };
 

6bb732500e79621f68e2653403ce00f99cbb0c1c
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-05-21 20:30:02 +0900

    [MCJ-1485] feat: 인증필요 경로 미들웨어

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('accessToken')?.value;
+
+  if (!token) {
+    const response = NextResponse.redirect(new URL('/login', request.url));
+    response.cookies.set(
+      'pendingRedirect',
+      decodeURIComponent(request.nextUrl.pathname),
+      {
+        path: '/',
+        maxAge: 60 * 5,
+        sameSite: 'strict',
+      },
+    );
+    return response;
+  }
+}
+
+export const config = {
+  // 비로그인 사용자 접근을 막는 경로 패턴
+  matcher: ['/item/:path*/join', '/payment/:path*'],
+};

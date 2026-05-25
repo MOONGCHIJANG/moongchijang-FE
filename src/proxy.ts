e32c6a9c0890085b2c60f26e9cfb9fcdd8e80475
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-05-25 19:59:59 +0900

    [MCJ-1467] feat: proxy 관련 개선

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+/*
+ * 보호 경로 추가 방법:
+ *   1. matcher 배열에 경로 패턴 추가(정확한 경로 패턴, 이 부분에서만 proxy 가 실행됨)
+ *   2. protectedPaths 배열에 경로 prefix 추가(접근 제어할 경로의 공통 prefix)
+ */
+
 export function proxy(request: NextRequest) {
   const token = request.cookies.get('accessToken')?.value;
   const { pathname } = request.nextUrl;
@@ -9,9 +15,13 @@ export function proxy(request: NextRequest) {
     return NextResponse.redirect(new URL('/feed', request.url));
   }
 
-  if (!token) {
+  // 보호 경로 정보
+  const protectedPaths = ['/item', '/payment', '/notifications'];
+  const isProtected = protectedPaths.some((p) => pathname.startsWith(p));
+
+  if (isProtected && !token) {
     const response = NextResponse.redirect(new URL('/login', request.url));
-    response.cookies.set('pendingRedirect', request.nextUrl.pathname, {
+    response.cookies.set('pendingRedirect', pathname, {
       path: '/',
       maxAge: 60 * 5,
       sameSite: 'strict',
@@ -21,6 +31,11 @@ export function proxy(request: NextRequest) {
 }
 
 export const config = {
-  // 비로그인 사용자 접근을 막는 경로 패턴
-  matcher: ['/item/:path*/join', '/payment/:path*', '/notifications'],
+  matcher: [
+    '/item/:path*/join',
+    '/payment/:path*',
+    '/notifications',
+    '/login',
+    '/login/:path*',
+  ],
 };

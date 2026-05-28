257d454251edcb0f019698c785241278f1cebd69
jmlee2147 <jm2147@khu.ac.kr>
2026-05-28 18:51:29 +0900

    [MCJ-1542] fix: 개발환경에서 GA4 스크립트 및 PostHog 초기화 차단

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,22 +89,23 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col mx-auto max-w-110">
-        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
+        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID &&
+          process.env.NODE_ENV !== 'development' && (
+            <>
+              <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+                strategy="afterInteractive"
+              />
+              <Script id="google-analytics" strategy="afterInteractive">
+                {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
               `}
-            </Script>
-          </>
-        )}
+              </Script>
+            </>
+          )}
         <IconsSetup />
         <KakaoInit />
         <PostHogProvider>

8c383b36da84acbabd6596d2968f494fbf73d4b3
leemanjae02 <honthalg@gmail.com>
2026-05-28 14:51:11 +0900

    [MCJ-1554] ui: 푸터 주소지 업데이트 및 페이지별 하단 여백 조건부 적용

--- src/app/(pages)/(gnb)/favorite/page.tsx
+++ src/app/(pages)/(gnb)/favorite/page.tsx
@@ -1,11 +1,5 @@
 import { FavoriteClient } from './_components/FavoriteClient';
-import Footer from '@/components/Footer';
 
 export default function FavoritePage() {
-  return (
-    <div className="flex flex-col h-full">
-      <FavoriteClient />
-      <Footer hasBottomSticky={false} />
-    </div>
-  );
+  return <FavoriteClient />;
 }
--- src/app/(pages)/(gnb)/feed/page.tsx
+++ src/app/(pages)/(gnb)/feed/page.tsx
@@ -1,15 +1,11 @@
 import { Suspense } from 'react';
 import { FeedClient } from './_components/FeedClient';
 import { FeedSkeletonList } from './_components/FeedSkeleton';
-import Footer from '@/components/Footer';
 
 export default function FeedPage() {
   return (
-    <div className="flex flex-col">
-      <Suspense fallback={<FeedSkeletonList />}>
-        <FeedClient />
-      </Suspense>
-      <Footer hasBottomSticky={false} />
-    </div>
+    <Suspense fallback={<FeedSkeletonList />}>
+      <FeedClient />
+    </Suspense>
   );
 }

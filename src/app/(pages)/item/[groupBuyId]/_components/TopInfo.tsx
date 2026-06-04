645a7891805a4426ebeeba71cdc25eac0502a8b5
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-06-05 04:44:08 +0900

    [MCJ-1607] chore: 이미지 auto 스타일링 추가

--- src/app/(pages)/item/[groupBuyId]/_components/TopInfo.tsx
+++ src/app/(pages)/item/[groupBuyId]/_components/TopInfo.tsx
@@ -16,8 +16,7 @@ const TopInfo = ({ data }: Props) => {
   const [isShared, setIsShared] = React.useState(false);
 
   useEffect(() => {
-    const itemStatus =
-      new Date(data.deadline) > new Date() ? 'open' : 'closed';
+    const itemStatus = new Date(data.deadline) > new Date() ? 'open' : 'closed';
     logEvent('view_item', {
       item_id: data.id,
       item_name: data.productName,
@@ -38,6 +37,7 @@ const TopInfo = ({ data }: Props) => {
             width={400}
             height={260}
             className="h-65 w-auto rounded-3xlarge bg-gray-100 object-cover"
+            style={{ width: 'auto' }}
             loading="eager"
           />
         ) : (

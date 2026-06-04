642f5b9dcbf52b11f697d3c3db9c399bfa18cfc6
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-06-05 04:56:35 +0900

    [MCJ-1607] chore: 공구 참여 텍스트 부분 정렬

--- src/app/(pages)/item/[groupBuyId]/join/_components/ItemSummary.tsx
+++ src/app/(pages)/item/[groupBuyId]/join/_components/ItemSummary.tsx
@@ -11,18 +11,20 @@ const ItemSummary = ({ groupBuy }: Props) => {
     <div className="p-p5 flex flex-col gap-1 rounded-2xlarge bg-surface-white border border-border-subtle">
       <p className="heading-md-bold">픽업 안내</p>
       <div className="flex flex-col divide-y divide-dashed divide-border-subtle">
-        <div className="pt-g3 pb-p5 flex justify-between">
-          <div className="flex gap-g3 items-center">
+        <div className="pt-g3 pb-p5 flex justify-between gap-g3">
+          <div className="flex shrink-0 gap-g3 items-center">
             <Icon
               icon="teenyicons:pin-alt-solid"
               className="w-4 h-4 text-icon-primary"
             />
             <p className="text-text-brand body-md-bold">픽업 장소</p>
           </div>
-          <p className="body-md-regular">{groupBuy.pickupLocation}</p>
+          <p className="min-w-0 flex-1 text-right body-md-regular wrap-break-word line-clamp-2">
+            {groupBuy.pickupLocation}
+          </p>
         </div>
-        <div className="pt-g3 pb-p5 flex justify-between">
-          <div className="flex gap-g3 items-center">
+        <div className="pt-g3 pb-p5 flex justify-between gap-g3">
+          <div className="flex shrink-0 gap-g3 items-center">
             <Icon
               icon="mingcute:time-fill"
               className="w-4 h-4 text-icon-primary"
